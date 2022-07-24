85e3f7f878845db172547305e5def2d206b10c32
miguel <miguelguevaraalejandro@gmail.com>
2022-07-24 17:23:46 -0500

    create model - controller usuario

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -10,18 +10,15 @@ const app=express();
 //Configurando CORS
 app.use(cors());
 
+//Lectura y parseo del body
+app.use(express.json());
+
 //Base de datos
 dbConnection();
 
 //Rutas
-app.get('/',(req,res)=>{
+app.use('/api/usuarios',require('./routes/usuarios'));
 
-    res.json({
-        ok:true,
-        msg:'Hola mundo'
-    })
-
-})
 
 app.listen(process.env.PORT,()=>{
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
